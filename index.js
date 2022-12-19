9b088c19b8fbcf830f7813fb3a233d29b25d1008
shishirbhargav09 <sbhargav2000@gmail.com>
2022-12-19 23:38:55 +0530

    cors

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use('/api', routes)
 // app.get("/",(req,res) => { 
